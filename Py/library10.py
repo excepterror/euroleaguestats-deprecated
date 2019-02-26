b49e4db06a9e7bd4c0fb31cb7de5346663139476
excepterror <thomas.sfondylis@gmail.com>
2019-02-26 22:58:49 +0200

    Add Network detection methods

--- Py/library10.py
+++ Py/library10.py
@@ -3,6 +3,47 @@ import socket
 from kivy.uix.label import Label
 from kivy.graphics import Color, RoundedRectangle
 from kivy.uix.button import ButtonBehavior
+from kivy.uix.popup import Popup
+from kivy.uix.floatlayout import FloatLayout
+
+
+# from jnius import autoclass
+
+
+def resolve_connectivity():
+    available_net = True  # check_network_availability()
+    if available_net:
+        available_site = is_connected('www.euroleague.net')
+        if available_site:
+            return True
+        else:
+            message = "euroleague.net could not be reached. Please, check your network connection."
+            return message
+    else:
+        message = 'Could not detect any network. Please, connect to a network first.'
+        return message
+
+
+def check_network_availability():
+    Context = autoclass('android.content.Context')
+    NetworkCapabilities = autoclass('android.net.NetworkCapabilities')
+    PythonActivity = autoclass('org.kivy.android.PythonActivity')
+    activity = PythonActivity.mActivity
+
+    # Create an instance of :cls: ConnectivityManager
+    con_mgr = activity.getSystemService(Context.CONNECTIVITY_SERVICE)
+    # Return the Network object corresponding to the currently active default data network.
+    network = con_mgr.getActiveNetwork()
+    # Call the NetworkCapabilities java class for our current Network object.
+    capabilities = con_mgr.getNetworkCapabilities(network)
+    # Check if the Network object is not null and verify the type of available network.
+    if capabilities is not None and (
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) or capabilities.hasTransport(
+        NetworkCapabilities.TRANSPORT_CELLULAR)):
+        return True
+    else:
+        return False
+
 
 def is_connected(hostname):
     # Called by :cls: 'Standings' - main.py.
@@ -11,12 +52,13 @@ def is_connected(hostname):
         # a DNS listening
         host = socket.gethostbyname(hostname)
         # connect to the host -- tells us if the host is actually
-        # reachable
-        s = socket.create_connection((host, 80), 2)
-        s.shutdown(2)
+        # reachable -- Port: 80 = HTTP, 53 = DNS, 10 sec = timeout
+        # :meth: gethostbyname returns an IPv4 address (numeric)
+        s = socket.create_connection((host, 80), 10)
+        s.shutdown(2)  # 2 = SHUT_RDWR
         s.close()
         return True
-    except:
+    except OSError:
         pass
     return False
 
@@ -39,3 +81,33 @@ class GotItButton(ButtonBehavior, Label):
     def update_rect(self, *args):
         self.rect.pos = self.pos
         self.rect.size = self.size
+
+
+class ConnWarnPopup(Popup):
+    def __init__(self, message, **kwargs):
+        super(ConnWarnPopup, self).__init__(**kwargs)
+
+        layout = FloatLayout()
+
+        self.content = layout
+        self.title = "Connection Error"
+        self.title_size = "17sp"
+        self.title_color = [.2, .6, .8, 1]
+        self.title_font = "Roboto-Regular"
+        self.separator_color = [.2, .6, .8, 1]
+        # self.background = "atlas://data/images/defaulttheme/textinput"
+        self.size_hint = [.9, .35]
+        self.pos_hint = {'center_x': .5, 'center_y': .5}
+        self.auto_dismiss = True
+
+        message = Label(text=message, font_size="17sp",
+                        color=(1, .4, 0, 1),
+                        size_hint=[1, .2],
+                        pos_hint={'center_x': .5, 'center_y': .55},
+                        halign="center",
+                        valign="middle")
+        message.bind(width=lambda *x: message.setter("text_size")(message, (message.width, None)),
+                     texture_size=lambda *x: message.setter("height")(message, message.texture_size[1]))
+
+        for w in [message]:
+            layout.add_widget(w)
